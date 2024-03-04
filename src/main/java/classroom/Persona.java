d10a4e803bd857ad56b625bbba065e22ce4dd9d3
sacardonaf <sacardonaf@unal.edu.co>
2024-03-04 14:52:06 -0500

    Solución taller 4 Java
    Aquí adjunto la solución que le di al problema de la semana relacionado con depurar el código únicamente modificando las clases: Asignatura, Grupo, Persona y Tipo

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,13 +2,18 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    long cedula=1;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
+    }
+    
+    public Persona() {
+    	this.cedula=0;
+    	this.nombre=null;
+    	totalPersonas++;
     }
 
     public Persona(long cedula, String nombre) {
@@ -30,7 +35,7 @@ public class Persona {
     }
 
     public Persona(String nombre) {
-        this.nombre = "";
+        this.nombre = nombre;
         totalPersonas++;
     }
     
